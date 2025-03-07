ce019f03ffb2f79d2af20f2cca12c472d753b002
Got <souvanxay38@gmail.com>
2025-03-07 09:46:51 +0100

    Fix css in ghpages.yml

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     root: "./src",
+    base: "/UniBot/",  // 🔹 Replace with your actual GitHub repository name
     build: {
         outDir: "../dist",
         emptyOutDir: true,
